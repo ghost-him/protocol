cd6809b18dac2d918903dfeaf387aa8e9fbee458
ghost_him <70733461+ghost-him@users.noreply.github.com>
2023-08-29 14:30:22 +0800

    Update protocolOption.h
    修改了一些头文件并删除了无用的宏定义

--- protocol/protocolOption.h
+++ protocol/protocolOption.h
@@ -2,10 +2,8 @@
 #include <string>
 #include <deque>
 #include <bitset>
-
-#define PROTOCOL_SIZE 4096
-#define PROTOCOL_TYPE_SIZE 50
-
+#include <cmath>
+#include <cstring>
 
 enum protocolType {
 	UINT,
@@ -27,20 +25,20 @@ class ProtocolOption {
 	friend class ProtocolBuilder;
 public:
 	ProtocolOption();
-	// »ñÈ¡µ¥´Î·¢ËÍµÄ×î´ó³¤¶È
+	// è·å–å•æ¬¡å‘é€çš„æœ€å¤§é•¿åº¦
 	unsigned int size();
 
-	// Ìí¼Ó£¬
-	// Õ¼ÓÃµÄ×Ö½Ú³¤¶È£¬ĞÅÏ¢ÀàĞÍ£¬ÃèÊö
+	// æ·»åŠ ï¼Œ
+	// å ç”¨çš„å­—èŠ‚é•¿åº¦ï¼Œä¿¡æ¯ç±»å‹ï¼Œæè¿°
 	bool append(unsigned int length, protocolType type, std::string description = "");
 
 private:
-	// ¿ØÖÆĞÅÏ¢
+	// æ§åˆ¶ä¿¡æ¯
 	std::bitset<1> _control;
 
 	std::deque<_Option> _optionList;
-	// ±íÃ÷Ö¸¶¨ÏûÏ¢³¤¶ÈµÄÏÂ±ê
+	// è¡¨æ˜æŒ‡å®šæ¶ˆæ¯é•¿åº¦çš„ä¸‹æ ‡
 	unsigned int _size_index;
-	// ±íÃ÷µ¥¸öÏûÏ¢µÄ×î´ó³¤¶È
+	// è¡¨æ˜å•ä¸ªæ¶ˆæ¯çš„æœ€å¤§é•¿åº¦
 	unsigned int _size;
 };

cd8a9f73e7c47dbcf65ab4541bb8ffadcafde402
ghost_him <70733461+ghost-him@users.noreply.github.com>
2024-01-12 21:28:12 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 std::shared_ptr<ProtocolOption> option = std::make_shared<ProtocolOption>();
 
 void init() {
-    option->append(1, UINT, "版本号");
-    option->append(1, UINT, "消息类型");
-    option->append(4, UINT, "总长度");
-    option->append(2, UINT, "消息组");
-    option->append(4, UINT, "偏移量");
-    option->append(20, STR, "说明信息");
+    option->append(1, INT, "版本号");
+    option->append(1, INT, "消息类型");
+    option->append(4, INT, "总长度");
+    option->append(2, INT, "消息组");
+    option->append(4, INT, "偏移量");
+    option->append(8, STR, "说明信息");
     option->append(3, SIZE, "消息长度");
 }
 
@@ -21,20 +21,20 @@ int main() {
     // 初始化生成器
     ProtocolBuilder builder;
     // 加载协议的格式
-    builder.load(option);
+    builder.setProtocolOption(option);
     // 设置头标记
-    builder.set_head(0, 1);     // 版本号          1
-    builder.set_head(1, 1);     // 消息类型        1
-    builder.set_head(2, 20);    // 总长度          20
-    builder.set_head(3, 0);     // 消息组          0
-    builder.set_head(4, 0);     // 当前消息的偏移量 0
-    builder.set_head(5, "支持字符");     // 说明信息 0
+    builder.setHeaderField(0, 1);     // 版本号          1
+    builder.setHeaderField(1, 1);     // 消息类型        1
+    builder.setHeaderField(2, 20);    // 总长度          20
+    builder.setHeaderField(3, 0);     // 消息组          0
+    builder.setHeaderField(4, 0);     // 当前消息的偏移量 0
+    builder.setHeaderField(5, "12345678");     // 说明信息 0
 
 
     // 发送a数组
     char a[3] = "10";
     // 生成可用于发送的协议 
-    auto proto = builder.set_data(a, 2);
+    auto proto = builder.buildWithData(a, 2);
     
     /////////////////////////////////
     // 发送
@@ -43,9 +43,12 @@ int main() {
     // 传递的过程是字符流
     unsigned char* data = static_cast<unsigned char*>(proto->data());
 
+    std::bitset<8> bit;
     for (int i = 0; i < 50; i++) {
-        printf("%x ", data[i]);
+        bit = data[i];
+        std::cout << bit << " ";
     }
+    std::cout << std::endl;
 
     //////////////////////////////////////////////
     // 
@@ -56,26 +59,27 @@ int main() {
     // 解码器
     ProtocolConverter converter;
     // 加载相同的协议格式
-    converter.load(option);
+    converter.setProtocolOption(option);
     // 解码
-    converter.convert(data);
+    converter.parseData(data);
     // 输出传递的原始数据
    // std::cout.write(static_cast<char *>(proto->data()), proto->size());
     // 输出首部信息
-    std::cout << "0 " << converter.get_head<unsigned int>(0) << std::endl;
-    std::cout << "1 " << converter.get_head<unsigned int>(1) << std::endl;
-    std::cout << "2 " << converter.get_head<unsigned int>(2) << std::endl;
-    std::cout << "3 " << converter.get_head<unsigned int>(3) << std::endl;
-    std::cout << "4 " << converter.get_head<unsigned int>(4) << std::endl;
-    std::cout << "5 " << converter.get_head<std::string>(5) << std::endl;
-    std::cout << "6 " << converter.get_head<unsigned int>(6) << std::endl;
+    std::cout << "0 " << std::get<int>(converter.getHeaderField(0)) << std::endl;
+    std::cout << "1 " << std::get<int>(converter.getHeaderField(1)) << std::endl;
+    std::cout << "2 " << std::get<int>(converter.getHeaderField(2)) << std::endl;
+    std::cout << "3 " << std::get<int>(converter.getHeaderField(3)) << std::endl;
+    std::cout << "4 " << std::get<int>(converter.getHeaderField(4)) << std::endl;
+
+    std::cout << "5 " << std::get<std::string>(converter.getHeaderField(5)) << std::endl;
+    std::cout << "6 " << std::get<int>(converter.getHeaderField(6)) << std::endl;
     // 开一个空间，用于存放数据部分
     char b[5] = {};
     // 将数据部分放到b数组中
-    converter.get_data(b);
+    converter.copyParsedData(b);
     std::cout << "解析出数据内容为 ";
     // 输出b的内容
     std::cout << b << std::endl;
-
+    
      return 0;
 }
